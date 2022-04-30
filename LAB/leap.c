e67cdea85a81787bd87d4bc0769d67757a3917c5
Atharv Salian <69430707+Atharv56@users.noreply.github.com>
2022-04-30 15:28:29 +0530

    correction in leap year

--- LAB/leap.c
+++ LAB/leap.c
@@ -4,8 +4,11 @@ int main(){
     int year;
     printf("Enter the year: ");
     scanf("%i", &year);
-    if (year%4 == 0){
+    if (year%400 == 0){
         printf("Its a Leap Year");
+    }
+    elif (year%100 == 0){
+        printf("Its not a leap year");
     }
     else{
         printf("Its not a Leap year");
