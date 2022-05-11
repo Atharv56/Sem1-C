3ee36a11791564efa35b0c0f8a84cb87a6fefcea
Atharv56 <69430707+Atharv56@users.noreply.github.com>
2022-05-11 11:59:49 +0530

    ncr

--- LAB/ncr.c
+++ LAB/ncr.c
@@ -1,5 +1,29 @@
 #include<stdio.h>
+int fact(int num);
+
 
 int main(){
     
+    int n, r;
+    int ncr;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    printf("Enter the value of r: ");
+    scanf("%d", &r);
+    ncr = fact(n) / (fact(r) * fact(n - r));
+    printf("\nValue of %dC%d is: %d", n, r, ncr);
+}
+
+int fact(int num){
+    int k = 1 , i;
+    if (num==0){
+        return (k);
+    }
+    else{
+        for (i=0; i<=num; ++i){
+            k = k*i;
+        }
+        
+    }
+    return (k);
 }
